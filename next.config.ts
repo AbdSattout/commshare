eab2b49c8b95b92a884e305fc28b422798d1a822
satabd321 <satabd321@gmail.com>
2025-06-25 10:49:37 +0000

    Fix images

--- next.config.ts
+++ next.config.ts
@@ -7,18 +7,19 @@ import redirects from './redirects.js'
 const NEXT_PUBLIC_SERVER_URL =
   (process.env.VERCEL_PROJECT_PRODUCTION_URL &&
     `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`) ||
+  process.env.NEXT_PUBLIC_DEV_URL ||
   process.env.__NEXT_PRIVATE_ORIGIN ||
   'http://localhost:3000'
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: [process.env.NEXT_PUBLIC_DEV_ORIGIN || ''],
   images: {
     remotePatterns: [
-      ...(process.env.NODE_ENV === 'development' ? [{
-        hostname: '*',
-      }] : [NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        return new URL(item)
-      })),
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
+        return new URL(item + '/**')
+      }),
     ],
+    unoptimized: true
   },
   reactStrictMode: true,
   redirects,
